ceec3eca29b8abfd9e426202a227bb092967dab1
SengMing Tan <tansengming@gmail.com>
2018-11-21 16:39:39 -0500

    initial commit

---
+++ lib/rubocop/disallow/safe/navigation.rb
@@ -0,0 +1,12 @@
+require "rubocop/disallow/safe/navigation/version"
+
+module Rubocop
+  module Disallow
+    module Safe
+      module Navigation
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/rubocop/disallow/safe/navigation_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rubocop::Disallow::Safe::Navigation do
+  it "has a version number" do
+    expect(Rubocop::Disallow::Safe::Navigation::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
